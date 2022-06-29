9b8990fc97d8ae339d43724da316d7fa45690306
Phillip <Flip.dlankford@gmail.com>
2022-06-28 17:11:44 -0700

    solve 4.2 and makes 4.2 tests

--- chapter-4-exercises.js
+++ chapter-4-exercises.js
@@ -43,3 +43,48 @@ function sum(array) {
 */
 
 // Excercise 4.2 - Reversing an Array
+
+// produces new array
+function reverseArray(array) {
+    const reverse = [];
+    for(let i = array.length - 1; i >= 0; i--){
+        reverse.push(array[i]);
+    }
+    return reverse;
+}
+
+exports.reverseArray = reverseArray;
+
+// modifies given array
+function reverseArrayInPlace(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        const back = array[array.length - (i + 1)];
+        const front = array[i];
+        array[i] = back;
+        array[array.length - (i + 1)] = front;
+    }
+    return array;
+}
+
+exports.reverseArrayInPlace = reverseArrayInPlace;
+
+/* // 4.2 Solution
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+*/
+
+// Exercise 4.3 - A List
--- chapter-4-exercises.test.js
+++ chapter-4-exercises.test.js
@@ -1,6 +1,7 @@
-const { range, sum } = require("./chapter-4-exercises");
+const { range, sum, reverseArray, reverseArrayInPlace } = require("./chapter-4-exercises");
 
-// unit tests
+// 4.1 Tests
+// Unit Tests
 
 test("return array of numbers between range, inclusive", () => {
     expect(range(1,4)).toStrictEqual([1,2,3,4]);
@@ -12,10 +13,21 @@ test("return sum of array of numbers", () => {
     expect(sum([1,2,3,4])).toBe(10);
 });
 
-// integration tests
+// Integration Tests
 
 test("return the sum of a range", () => {
     expect(sum(range(1,10))).toBe(55);
     expect(sum(range(10,1))).toBe(55);
     expect(sum(range(1,10,2))).toBe(25);
+});
+
+// 4.2 Tests 
+test("produces reversed array", () => {
+    expect(reverseArray([1,2,3,4])).toStrictEqual([4,3,2,1]);
+    expect(reverseArray([1,2,3,4,5])).toStrictEqual([5,4,3,2,1]);
+});
+
+test("produces reversed array", () => {
+    expect(reverseArrayInPlace([1,2,3,4])).toStrictEqual([4,3,2,1]);
+    expect(reverseArray([1,2,3,4,5])).toStrictEqual([5,4,3,2,1]);
 });
