656c827b6a01ced1c2d859f92c8504489a0c4b17
Phillip <Flip.dlankford@gmail.com>
2022-06-30 13:12:45 -0700

    5.2 and 5.3 exercises + tests

--- chapter-5-exercises.js
+++ chapter-5-exercises.js
@@ -1,7 +1,52 @@
 // Exercise 5.1 - Flattening
+
 // Flatten an array of arrays to a single array
 function flatten(array) {
     return array.reduce( (prev, current) => [...prev, ...current] );
 }
 
 exports.flatten = flatten;
+
+// Exercise 5.2 - Your Own Loop
+// write higher-order function
+// arguments: value, test function, update function, body function
+
+function loop(val, testFunc, updateFunc, bodyFunc) {
+    // run test function on current loop value. Stop if returns false
+    // Call the body function giving it the current value
+    // Call the update function to create a new value and starts from the beginning
+    if (!testFunc(val)) return false;
+    bodyFunc(val);
+    loop(updateFunc(val), testFunc, updateFunc, bodyFunc);
+    // Book Solution 8ms faster
+    // for (let value = start; testFunc(value); value = updateFunc(value)) {
+    //     bodyFunc(value);
+    //   }
+}
+
+exports.loop = loop;
+
+// Exercise 5.3 - Everything
+// Array's have an every method 
+// returns true when given function returns true for every element in the array
+// Some is || that acts on arrays 
+// Every is the && that acts on arrays
+
+// using loop 
+function everyLoop(array, condition) {
+    for(let i = 0; i < array.length; i++) {
+        if(!condition(array[i])) return false;
+    }
+    return true;
+};
+
+exports.everyLoop = everyLoop;
+
+// using some
+function everySome(array, condition) {
+    // De Morgan's laws
+    // a && b equals !(!a || !b)
+    return !array.some(element => !condition(element));
+};
+
+exports.everySome = everySome;    
--- chapter-5-exercises.test.js
+++ chapter-5-exercises.test.js
@@ -1,5 +1,25 @@
-const { flatten } = require("./chapter-5-exercises");
+const { flatten, loop, everyLoop, everySome } = require("./chapter-5-exercises");
 
 test("convert array of arrays to single array", () => {
     expect(flatten([[1,2,3],[4,5,6],[7,8,9]])).toStrictEqual([1,2,3,4,5,6,7,8,9]);
+});
+
+test("apply given function to each value that fulfills a condition", () => {
+    const logSpy = jest.spyOn(console, "log");
+
+    loop(3, n => n > 0, n => n - 1, console.log)
+
+    expect(logSpy).toHaveBeenCalledWith(1);
+    expect(logSpy).toHaveBeenCalledWith(2);
+    expect(logSpy).toHaveBeenCalledWith(3);
+});
+
+test("return true for all array elements against given test w/ loop", () => {
+    expect(everyLoop([5,6,7], n => n > 1)).toBe(true);
+    expect(everyLoop([5,6,1,0], n => n > 1)).toBe(false);
+});
+
+test("return true for all array elements against given test w/ some", () => {
+    expect(everySome([5,6,7], n => n > 1)).toBe(true);
+    expect(everySome([5,6,1,0], n => n > 1)).toBe(false);
 });
