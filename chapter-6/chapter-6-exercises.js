d87ec50a6a816802478334123c3728ad248967f5
Phillip <Flip.dlankford@gmail.com>
2022-07-18 13:42:03 -0700

    document 6.1 problem in comments

--- chapter-6/chapter-6-exercises.js
+++ chapter-6/chapter-6-exercises.js
@@ -1,3 +1,9 @@
+// Exercise 6.1 - A Vector Type
+// write a Vector class that takes x and y
+// define two methods, plus and minus that take a vector
+// and return a new vector with the points added or subtracted
+// add a getter property that finds the points distance from (0,0)
+
 class Vector {
     constructor(x,y) {
         this.x = x;
